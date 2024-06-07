d22e8ac37ab06e86d6dc300cd729de24566b14ab
ezoa <ezoaavotchi@gmail.com>
2024-06-07 16:51:29 +0900

    django

---
+++ mysite/polls/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+
+from .models import Question
+
+# Create your views here.
+
+def index(request):
+
+    # return  HttpResponse("Hello this is my first code")
+
+    latest_question_list= Question.objects.order_by("-pub_date")[:5]
+    template=loader.get_template("polls/index.html")
+
+    # output =",".join([q.question_text for  q in latest_question_list])
+    # return HttpResponse(output)
+
+    context={
+        "latest_question_list":latest_question_list,
+    }
+    # return HttpResponse(template.render(context,request))
+    return render(request,"polls/index.html",context)
+
+
+def detail(request,question_id):
+    try:
+        question= Question.objects.get(pk=question_id)
+        print(question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    
+    # return HttpResponse(" you are looking at the question details %s" % question_id)
+    return render(request,"polls/detail.html",{"question":question})
+
+
+def results(request,question_id):
+    response="You are looking at the results of questions %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You 're voting on question %s" %question_id)
